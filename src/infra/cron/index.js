5a15da864666f742b5743d8179992e422c1fd00f
Vitor Araujo <vitorgamer58@outlook.com>
2023-06-28 23:15:25 -0300

    Adiciona validação para as expressões cron
    Adiciona mensagem personalizada no console

--- src/infra/cron/index.js
+++ src/infra/cron/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
 const { CronJob } = require("cron");
+const cronValidator = require("cron-validator");
+const cronstrue = require("cronstrue/i18n");
 const enviarFechamento = require("../../domain/usecases/enviarFechamento");
 
 const CRON_SCHEDULE = "30 18 * * 1-5";
@@ -9,9 +11,14 @@ const CRON_SCHEDULE = "30 18 * * 1-5";
  * @param {Telegraf<Context<Update>>} bot - A instância do Telegraf usada para interagir com o bot do Telegram.
  *
  * @throws {Error} Se a instância de Telegraf não for passada como argumento.
+ * @throws {Error} Se a expressão cron for inválida.
  */
 const cronJobs = (bot) => {
   if (!bot) throw new Error("Instância do bot não passada");
+
+  const isCronValid = cronValidator.isValidCron(CRON_SCHEDULE);
+
+  if (!isCronValid) throw new Error(`Expressão cron ${CRON_SCHEDULE} inválida`);
 
   new CronJob(
     CRON_SCHEDULE,
@@ -34,7 +41,8 @@ const cronJobs = (bot) => {
     "America/Sao_Paulo"
   );
 
-  console.log("⌚ CronJob iniciado com sucesso");
+  const cronPortugues = cronstrue.toString(CRON_SCHEDULE, { locale: "pt_BR" });
+  console.log(`⌚ CronJob configurado para rodar ${cronPortugues}`);
 };
 
 const enviaMensagemErro = async (bot, mensagem) => {
