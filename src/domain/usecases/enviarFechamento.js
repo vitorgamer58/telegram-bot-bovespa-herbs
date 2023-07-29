5db44bf7b23f2027eb103548678e75993247159b
Vitor Araujo <vitorgamer58@outlook.com>
2023-07-29 20:47:47 -0300

    Adiciona tratamento de clientes bloqueados no uc enviarFechamento
    Quando um cliente bloqueia o bot, ele deve ser limpo da base de dados.

--- src/domain/usecases/enviarFechamento.js
+++ src/domain/usecases/enviarFechamento.js
@@ -4,10 +4,12 @@ const fechamento = require("./fechamento")
 const HandleBars = require("handlebars")
 const { fechamentoTemplate } = require("../templates")
 const { herbarium } = require("@herbsjs/herbarium")
+const alteraCadastro = require("./alteraCadastro")
 
 const dependency = {
   obterClientes,
   fechamento,
+  alteraCadastro,
 }
 
 const enviarFechamento = (injection) =>
@@ -36,7 +38,7 @@ const enviarFechamento = (injection) =>
     }),
 
     "Obtem o fechamento do dia": step(async (ctx) => {
-      const fechamentoUc = ctx.di.fechamento()
+      const fechamentoUc = ctx.di.fechamento(ctx.di)
 
       await fechamentoUc.authorize()
 
@@ -62,6 +64,7 @@ const enviarFechamento = (injection) =>
     "Envia a mensagem de fechamento para todos os clientes": step(async (ctx) => {
       const { clientes, mensagem } = ctx.data
       const { bot } = ctx.di
+      const errors = []
 
       await Promise.all(
         clientes.map(async (cliente) => {
@@ -71,9 +74,12 @@ const enviarFechamento = (injection) =>
             console.log(
               `Erro ao enviar mensagem para o cliente ${cliente.chat_id}: ${error.message}`
             )
+            errors.push({ cliente, error })
           }
         })
       )
+
+      ctx.data.errors = errors
 
       return Ok()
     }),
@@ -93,13 +99,47 @@ const enviarFechamento = (injection) =>
             try {
               await bot.telegram.sendMessage(cliente.chat_id, mensagem)
             } catch (error) {
-              console.log(`Erro ao enviar mensagem para o cliente ${cliente.id}: ${error.mensagem}`)
+              console.log(
+                `Erro ao enviar mensagem de atualização para o cliente ${cliente.id}: ${error.mensagem}`
+              )
             }
           })
         )
 
         return Ok()
       }),
+
+    "Trata os erros": step(async (ctx) => {
+      const { errors } = ctx.data
+
+      const clientesComBloqueio = errors
+        .filter(({ error }) => error?.response?.error_code === 403)
+        .map(({ cliente }) => cliente)
+
+      ctx.data.clientesParaExcluir = clientesComBloqueio
+      return Ok()
+    }),
+
+    "Exclui do cadastro os usuários que bloquearam o bot": step(async (ctx) => {
+      const { clientesParaExcluir } = ctx.data
+      const { alteraCadastro } = ctx.di
+
+      for (const cliente of clientesParaExcluir) {
+        try {
+          const alteraCadastroInstance = alteraCadastro(ctx.di)
+
+          await alteraCadastroInstance.authorize()
+
+          const ucResponse = await alteraCadastroInstance.run(cliente)
+
+          if (ucResponse.isErr) console.log(`Erro ao apagar cadastro: ${ucResponse.err}`)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
+      return Ok()
+    }),
   })
 
 module.exports = herbarium.usecases
